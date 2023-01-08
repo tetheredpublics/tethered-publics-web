cb32dd9dab0b622abe00be8b286576862db7b753
Karl Sims <karl@citymapper.com>
2023-01-08 23:28:17 +0000

    main - add open call page

---
+++ src/routes/open-call/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import SEO from '$lib/SEO.svelte';
+</script>
+<SEO title="About" />
+
+<div class="header-spacer"></div>
+<div class="container">
+    <div class="text-block">
+        <h1>Open Call 2022</h1>
+        <p class="large-paragraph">This Open Call closed on the 20th of June 2022.</p>
+        <p class="">Keep your eyes open for another in 2023... 👀</p>
+    </div>
+</div>
